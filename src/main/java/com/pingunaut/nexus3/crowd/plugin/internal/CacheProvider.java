21bf7e370938ed4f1007cd5e1b1b5485ca99bf20
Martin Spielmann <martin.spielmann@pingunaut.com>
2017-04-22 10:37:26 +0200

    remove nexus internal dependency. fixes #29

--- src/main/java/com/pingunaut/nexus3/crowd/plugin/internal/CacheProvider.java
+++ src/main/java/com/pingunaut/nexus3/crowd/plugin/internal/CacheProvider.java
@@ -4,8 +4,6 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-import javax.cache.configuration.Configuration;
-import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
@@ -14,11 +12,10 @@ import org.ehcache.Cache;
 import org.ehcache.CacheManager;
 import org.ehcache.ValueSupplier;
 import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.CacheManagerBuilder;
 import org.ehcache.config.builders.ResourcePoolsBuilder;
-import org.ehcache.core.config.BaseCacheConfiguration;
 import org.ehcache.expiry.Duration;
 import org.ehcache.expiry.Expiry;
-import org.sonatype.nexus.cache.internal.ehcache.EhCacheManagerProvider;
 
 @Singleton
 @Named("CrowdCacheProvider")
@@ -31,14 +28,15 @@ public class CacheProvider {
     // cache lifetime 15m
     private static final int TTL_SECONDS = 3600 / 4;
 
-    private EhCacheManagerProvider provider;
-
     private Cache<String, CachedToken> tokenCache;
     private Cache<String, Set> responseCache;
 
-    @Inject
-    public CacheProvider(EhCacheManagerProvider provider) {
-        this.provider = provider;
+    private CacheManager manager;
+
+    public CacheProvider() {
+        manager = CacheManagerBuilder.newCacheManagerBuilder().build();
+        manager.init();
+
         tokenCache = createTokenCache();
         responseCache = createResponseCache();
     }
@@ -48,54 +46,12 @@ public class CacheProvider {
     }
 
     private Cache<String, Set> createResponseCache() {
-        CacheConfigurationBuilder<String, Set> c =
-                CacheConfigurationBuilder.newCacheConfigurationBuilder(String.class, Set.class, ResourcePoolsBuilder
-                        .heap(100))
-                        .withExpiry(new Expiry<String, Set>() {
-                            @Override
-                            public Duration getExpiryForCreation(String key, Set value) {
-                                return Duration.of(TTL_SECONDS, TimeUnit.SECONDS);
-                            }
-
-                            @Override
-                            public Duration getExpiryForAccess(String key, ValueSupplier<? extends Set> value) {
-                                return null;  // Keeping the existing expiry
-                            }
-
-                            @Override
-                            public Duration getExpiryForUpdate(String key, ValueSupplier<? extends Set> oldValue, Set newValue) {
-                                return null;  // Keeping the existing expiry
-                            }
-                        });
-
-        return cacheManager().createCache(RESPONSES_CACHE_NAME, c);
+        return manager.createCache(RESPONSES_CACHE_NAME, createCacheConfig(String.class, Set.class));
     }
+
 
     private Cache<String, CachedToken> createTokenCache() {
-        CacheConfigurationBuilder<String, CachedToken> c =
-                CacheConfigurationBuilder.newCacheConfigurationBuilder(String.class, CachedToken.class, ResourcePoolsBuilder
-                        .heap(100))
-                        .withExpiry(new Expiry<String, CachedToken>() {
-                            @Override
-                            public Duration getExpiryForCreation(String key, CachedToken value) {
-                                return Duration.of(TTL_SECONDS, TimeUnit.SECONDS);
-                            }
-
-                            @Override
-                            public Duration getExpiryForAccess(String key, ValueSupplier<? extends CachedToken> value) {
-                                return null;  // Keeping the existing expiry
-                            }
-
-                            @Override
-                            public Duration getExpiryForUpdate(String key, ValueSupplier<? extends CachedToken> oldValue, CachedToken newValue) {
-                                return null;  // Keeping the existing expiry
-                            }
-                        });
-        return cacheManager().createCache(TOKEN_CACHE_NAME, c);
-    }
-
-    private CacheManager cacheManager(){
-        return (CacheManager)provider.get();
+        return manager.createCache(TOKEN_CACHE_NAME, createCacheConfig(String.class, CachedToken.class));
     }
 
     public Optional<CachedToken> getToken(String username) {
@@ -111,5 +67,26 @@ public class CacheProvider {
 
     public void putGroups(String username, Set<String> groups) {
         responseCache.put(GROUPS_KEY_PREFIX + username, groups);
+    }
+
+    private <K, V> CacheConfigurationBuilder<K, V> createCacheConfig(Class<K> keyClass, Class<V> valueClass) {
+        return CacheConfigurationBuilder.newCacheConfigurationBuilder(keyClass, valueClass, ResourcePoolsBuilder
+                .heap(100))
+                .withExpiry(new Expiry<K, V>() {
+                    @Override
+                    public Duration getExpiryForCreation(K key, V value) {
+                        return Duration.of(TTL_SECONDS, TimeUnit.SECONDS);
+                    }
+
+                    @Override
+                    public Duration getExpiryForAccess(K key, ValueSupplier<? extends V> value) {
+                        return null;  // Keeping the existing expiry
+                    }
+
+                    @Override
+                    public Duration getExpiryForUpdate(K key, ValueSupplier<? extends V> oldValue, V newValue) {
+                        return null;  // Keeping the existing expiry
+                    }
+                });
     }
 }
