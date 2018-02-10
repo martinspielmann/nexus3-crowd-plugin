ac06678bc4f6e9ce364b01db7ed6e284b321e6af
Martin <mail@martinspielmann.de>
2018-02-10 16:27:32 +0100

    Add and fix unit test (#42)
    * add jacoco plugin
    
    * add unit tests

--- src/main/java/com/pingunaut/nexus3/crowd/plugin/internal/CachingNexusCrowdClient.java
+++ src/main/java/com/pingunaut/nexus3/crowd/plugin/internal/CachingNexusCrowdClient.java
@@ -80,11 +80,11 @@ public class CachingNexusCrowdClient implements NexusCrowdClient {
 	}
 
 	// handle CloseableHttpResponse properly
-	private <T> T executeQuery(final HttpUriRequest request, final ResponseHandler<? extends T> responseHandler) {
+	protected <T> T executeQuery(final HttpUriRequest request, final ResponseHandler<? extends T> responseHandler) {
 		try {
 			return getClient().execute(host, request, responseHandler);
 		} catch (IOException e) {
-			LOGGER.error("error executng query", e);
+			LOGGER.error("error executing query", e);
 			return null;
 		}
 	}
@@ -110,16 +110,8 @@ public class CachingNexusCrowdClient implements NexusCrowdClient {
 	@Override
 	public boolean authenticate(UsernamePasswordToken token) {
 		// check if token is cached
-		if(authCacheEnabled) {
-			Optional<CachedToken> cachedToken = cache.getToken(token.getUsername());
-			if (cachedToken.isPresent()) {
-				// check password
-                boolean isPasswordValid = PasswordHasher.isPasswordCorrect(token.getPassword(), cachedToken.get().salt, cachedToken.get().hash);
-				if (isPasswordValid) {
-                    LOGGER.info("Authenticated using cached credentials");
-					return true;
-				}
-			}
+		if(isAuthCacheEnabled() && authenticateFromCache(token)){
+			return true;
 		}
 
 		// if authentication with cached value fails or is skipped, crowd and check auth
@@ -128,23 +120,34 @@ public class CachingNexusCrowdClient implements NexusCrowdClient {
 
 		if (StringUtils.hasText(authResponse)) {
             // authentication was successful
-            if(authCacheEnabled){
-                cache.putToken(token.getUsername(), createCachedToken(token.getPassword()));
+            if(isAuthCacheEnabled()){
+                getCache().putToken(token.getUsername(), createCachedToken(token.getPassword()));
             }
             return true;
 		}
-
-        // authentication failed
+		// authentication failed
         return false;
 	}
 
-	private static CachedToken createCachedToken(char[] input)  {
-		byte[] salt = PasswordHasher.getNextSalt();
-        byte[] hash = PasswordHasher.hash(input, salt);
-        return new CachedToken(hash, salt);
+	protected boolean authenticateFromCache(UsernamePasswordToken token) {
+		Optional<CachedToken> cachedToken = cache.getToken(token.getUsername());
+		if (cachedToken.isPresent()) {
+            // check password
+            boolean isPasswordValid = PasswordHasher.isPasswordCorrect(token.getPassword(), cachedToken.get().salt, cachedToken.get().hash);
+            if (isPasswordValid) {
+                LOGGER.info("Authenticated using cached credentials");
+				return true;
+            }
+        }
+		return false;
 	}
 
-	private CloseableHttpClient getClient() {
+	protected CachedToken createCachedToken(char[] input)  {
+		byte[] salt = PasswordHasher.getNextSalt();
+        return new CachedToken(PasswordHasher.hash(input, salt), salt);
+	}
+
+	protected CloseableHttpClient getClient() {
 		return client;
 	}
 
@@ -208,6 +211,18 @@ public class CachingNexusCrowdClient implements NexusCrowdClient {
 	}
 
 	private String restUri(String path) {
-		return String.format("%s/rest/usermanagement/1/%s", serverUri.toString(), path);
+		return String.format("%s/rest/usermanagement/1/%s", getServerUriString(), path);
 	}
+
+	protected String getServerUriString(){
+	    return serverUri.toString();
+    }
+
+    protected boolean isAuthCacheEnabled(){
+	    return authCacheEnabled;
+    }
+
+    protected CacheProvider getCache() {
+        return cache;
+    }
 }
